ab397799f6ef922f2f99ebc9dc0f6d238f6f2899
Benedict <benedictryan80@gmail.com>
2023-10-28 18:10:34 +0700

    use server component for Modal Routing

--- app/form-modal-routing/page.js
+++ app/form-modal-routing/page.js
@@ -1,8 +1,8 @@
-'use client';
+// 'use client';
 
 import dynamic from 'next/dynamic';
 import Link from 'next/link';
-import { useRouter, useSearchParams } from 'next/navigation';
+// import { useRouter, useSearchParams } from 'next/navigation';
 const Form = dynamic(() => import('../components/Form'), {
 	ssr: false
 });
@@ -10,15 +10,16 @@ const Modal = dynamic(() => import('../components/Modal'), {
 	ssr: false
 });
 
-const FormModalRouting = () => {
-	const router = useRouter();
-	const searchParams = useSearchParams();
+const FormModalRouting = ({ searchParams }) => {
+	// const router = useRouter();
+	// const searchParams = useSearchParams();
 
-	const handleShowFormModal = () => {
-		router.push(`/form-modal-routing?form_modal=y`);
-	};
+	// const handleShowFormModal = () => {
+	// 	router.push(`/form-modal-routing?form_modal=y`);
+	// };
 
-	const search = searchParams.get('form_modal');
+	// const search = searchParams.get('form_modal');
+	// searchParams && console.log(searchParams);
 
 	return (
 		<div>
@@ -28,13 +29,19 @@ const FormModalRouting = () => {
 			<br />
 			<br />
 
-			<div
+			{/* <div
 				onClick={handleShowFormModal}
 				className="text-2xl underline cursor-pointer"
 			>
 				Go to Form in Modal
-			</div>
-			{search && (
+			</div> */}
+			<Link
+				href="/form-modal-routing?form_modal=y"
+				className="text-2xl underline"
+			>
+				Go to Form in Modal
+			</Link>
+			{searchParams?.form_modal && (
 				<Modal>
 					<Form />
 				</Modal>
