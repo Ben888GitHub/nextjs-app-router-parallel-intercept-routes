f8243d4ca21eae67d1da30a84de162d099825913
Benedict <benedictryan80@gmail.com>
2023-10-28 17:58:17 +0700

    implement modal routing for Form Component

---
+++ app/form-modal-routing/form/page.js
@@ -0,0 +1,9 @@
+// import Form from '@/app/components/Form';
+import dynamic from 'next/dynamic';
+const Form = dynamic(() => import('@/app/components/Form'), {
+	ssr: false
+});
+
+const FormPage = () => <Form />;
+
+export default FormPage;
---
+++ app/form-modal-routing/page.js
@@ -0,0 +1,48 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+import Link from 'next/link';
+import { useRouter, useSearchParams } from 'next/navigation';
+const Form = dynamic(() => import('../components/Form'), {
+	ssr: false
+});
+const Modal = dynamic(() => import('../components/Modal'), {
+	ssr: false
+});
+
+const FormModalRouting = () => {
+	const router = useRouter();
+	const searchParams = useSearchParams();
+
+	const handleShowFormModal = () => {
+		router.push(`/form-modal-routing?form_modal=y`);
+	};
+
+	const search = searchParams.get('form_modal');
+
+	return (
+		<div>
+			<Link href="/form-modal-routing/form" className="text-2xl underline">
+				Go to Form
+			</Link>
+			<br />
+			<br />
+			{/* <Link href="/form-modal-routing" className="text-2xl underline">
+				Go to Form in Modal
+			</Link> */}
+			<div
+				onClick={handleShowFormModal}
+				className="text-2xl underline cursor-pointer"
+			>
+				Go to Form in Modal
+			</div>
+			{search && (
+				<Modal>
+					<Form />
+				</Modal>
+			)}
+		</div>
+	);
+};
+
+export default FormModalRouting;
